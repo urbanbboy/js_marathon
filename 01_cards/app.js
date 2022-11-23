bf7940a5fad2050c33f0c631f6db89ecd2dc9808
talant <talant.04asanov@gmail.com>
2022-11-23 08:49:41 +0600

    No changes

---
+++ 01_cards/app.js
@@ -0,0 +1,24 @@
+function slidePlugin(activeSlide = 0) {
+
+    const slides = document.querySelectorAll('.slide')
+
+    slides[activeSlide].classList.add('active')
+
+    for(const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearACtiveClasses()
+
+            slide.classList.add('active')
+        })
+    }
+
+    function clearACtiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
+
+}
+
+slidePlugin();
+
---
+++ 05_aim_game/app.js
@@ -0,0 +1,87 @@
+const startBtn = document.querySelector('#start');
+const timeList = document.querySelector('#time-list');
+const screens = document.querySelectorAll('.screen');
+const timeEl = document.querySelector('#time');
+const board = document.querySelector('#board');
+const colors = ['#6209b5', '#00e635', '#03aadd', '#d53200', '#dbca00']
+let time = 0;
+let score = 0;
+
+
+startBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    screens[0].classList.add('up')
+})
+
+timeList.addEventListener('click', event => {
+    if(event.target.classList.contains('time-btn')) {
+        time = parseInt(event.target.getAttribute('data-time'));
+        screens[1].classList.add('up')
+        startGame();
+    }
+})
+
+board.addEventListener('click', (event) => {
+    if(event.target.classList.contains('circle')) {
+        score++
+        event.target.remove()
+        createRandomCircle()
+    }
+})
+
+function startGame() {
+    setInterval(decreaseTime, 1000)
+    createRandomCircle()
+    setTime(time)
+
+
+}
+
+function decreaseTime() {
+    if(time === 0) {
+        finishGame()
+    } else {
+        let current = --time
+        if(current < 10) {
+            current = `0${current}`
+        }
+        setTime(current)
+    }
+
+}
+
+function setTime(value) {
+    timeEl.innerHTML = `00:${value}`
+}
+
+function finishGame() {
+
+    board.innerHTML = `<h1>Cчет: <span class="primary">${score}</span></h1>`
+    timeEl.parentNode.classList.add('hide')
+}
+
+function createRandomCircle() {
+    const circle = document.createElement('div')
+    const size = getRandomNumber(20, 60)
+    const {width, height} = board.getBoundingClientRect()
+    const x = getRandomNumber(0, width - size)
+    const y = getRandomNumber(0, height - size)
+
+    circle.classList.add('circle')
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${y}px`
+    circle.style.left = `${x}px`
+    circle.style.background = randomColors()
+
+    board.append(circle)
+}
+
+function getRandomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+function randomColors() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
